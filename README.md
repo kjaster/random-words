2c1bdc24b8fdc9e76ade8744d2a9dfe8c765075a
kjaster <kaster@schueler.hakzell.at>
2024-04-24 09:26:13 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# RandomWords
+## Config
+
+```php 
+namespace Kjaster\RandomWords;
+
+require 'vendor/autoload.php';
+```
+PSR-4 autoloading configured. Use "namespace Kjaster\RandomWords;" in src/
+Include the Composer autoloader with: require 'vendor/autoload.php';
---
+++ src/Sentence.php
@@ -0,0 +1,45 @@
+<?php
+namespace Kjaster\RandomWords;
+
+class Sentence
+{
+    private string $sentence ='';
+
+    /**
+     * Gets the original sentence
+     *
+     * @return string
+     */
+    public function getSentence(): string
+    {
+        return $this->sentence;
+    }
+
+    public function setSentence(string $sentence): void
+    {
+        $this->sentence = trim($sentence);
+    }
+
+    function __construct($sentence)
+    {
+        //$this->sentence=$sentence;
+        $this->setSentence($sentence);
+    }
+
+    public function getRandomSentence(): string{
+        $words = explode(' ', $this->sentence);
+
+        // more than one word
+        if(count($words)>0){
+
+            $result =shuffle($words);
+
+            if($result === true){
+                return implode(' ',$words);
+            }
+        }
+
+        return $this->sentence;
+    }
+
+}
